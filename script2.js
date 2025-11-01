15ccbe724dfbf1aec26d0e9cd38ce4001a682291
simrandharr <simrandhar62@gmail.com>
2025-11-01 16:30:01 +0530

    add changes

--- script2.js
+++ script2.js
@@ -10,19 +10,16 @@ document.querySelectorAll('.slider').forEach(slider => {
     images[i].classList.add("active");
   };
 
-  // ✅ Next button
   slider.querySelector(".next").addEventListener("click", () => {
     index = (index + 1) % images.length;
     showSlide(index);
   });
 
-  // ✅ Prev button
   slider.querySelector(".prev").addEventListener("click", () => {
     index = (index - 1 + images.length) % images.length;
     showSlide(index);
   });
 
-  // ✅ AUTO SLIDE every 4s
   setInterval(() => {
     index = (index + 1) % images.length;
     showSlide(index);
